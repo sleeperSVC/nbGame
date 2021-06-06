807131349e9b261c35d1b6375b58d036016dffbb
sleeperSVC <73264419+sleeperSVC@users.noreply.github.com>
2021-06-06 13:45:28 -0700

    purple bullets

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,10 +1,16 @@
 import java.awt.*;
 
 public class Bullet extends GameMovingObject {
+    //bullet colorings
+    Color purple1 = new Color(125, 52, 235);
+    Color purple2 = new Color(96, 30, 191);
+    Color purple3 = new Color(131, 79, 222);
+    Color current;
 
     public Bullet(int x, int y, int width, int height, double speedFactor, double bulletInaccuracy, int orientation, double pxV, double pyV) {
         super((orientation == 1 ? x + 32 : x), y + 17, width, height, 1, speedFactor);
         // don't get scared of this ^ ternary operator, its just the same as "if orientation == 1, then return x+32, else return x
+
         this.orientation = orientation;
         this.xV = pxV; // TODO initial velocity is the player's velocity. simulates inertia. kinda scuffed atm
         this.yV = pyV;
@@ -36,15 +42,29 @@ public class Bullet extends GameMovingObject {
         if (!collisionBox.intersects(GamePanel.frameCollisionBox) || xVMax <= 1) {
             isAlive = false;
         }
+
+        //updates color
+        int randColor = (int) (Math.random() * 3);
+        switch (randColor){
+            case 0:
+                current = purple1;
+                break;
+            case 1:
+                current = purple2;
+                break;
+            default:
+                current = purple3;
+                break;
+        }
     }
 
     @Override
     public void draw(Graphics g) {
         int randWidth = (int) (Math.random() * 5);
         int randHeight = (int) (Math.random() * 5);
-        g.setColor(Color.WHITE);
+        g.setColor(current);
         g.fillOval(x, y, width + randWidth, height + randHeight);
-        g.setColor(Color.BLACK);
+        g.setColor(current);
         g.drawOval(x, y, width + randWidth, height + randHeight);
     }
 }
